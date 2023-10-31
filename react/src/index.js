cf9c0f7bdfc40ad34a0f69f1e282c802872ced97
ShekharRedd <shekharreddy4558@gmail.com>
2023-10-31 10:53:32 +0530

    modifying the python react

---
+++ react/src/index.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import Route_1 from './Route_1';
+
+// Use createRoot to render the App component
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
+root.render(<Route_1 />);
+
+
+
+
