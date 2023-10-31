60ef1e9536e26546fcab4b11d51d16fe5efc172c
ShekharRedd <shekharreddy4558@gmail.com>
2023-10-31 10:39:52 +0530

    hello

---
+++ src/Add_Data.js
@@ -0,0 +1,129 @@
+import React from 'react'
+import { useState } from 'react'
+const Add_Data = () => {
+  const [employee_name ,setEmployee_name] =useState('')
+  const [department,setDepartment]=useState('')
+  const [job_title,setJob_title]=useState('')
+  const [email_address,setEmail_address]=useState('')
+  const [phone_number,setPhone_number]=useState(0)
+  const [address,setAddress]=useState('')
+  const [confirm_status,setConfirm_status]=useState('')
+
+  const Handle_employee_name=(event)=>{
+      setEmployee_name(event.target.value)
+  }
+  const Handle_department=(event)=>{
+      setDepartment((event.target.value))
+  }
+  const Handle_job_title=(event)=>{
+    setJob_title(event.target.value)
+  }
+
+  const Handle_email_address=(event)=>{
+    setEmail_address(event.target.value)
+    // console.log(result)
+  }
+  const Handle_phone_number=(event)=>{
+    setPhone_number(event.target.value)
+  }
+  const Handle_address=(event)=>{
+    setAddress(event.target.value)
+  }
+  
+  const Handle_submit=(event)=>{
+    event.preventDefault()
+
+    let send_data={
+      "employee_name":employee_name,
+      "department":department,
+      "job_title":job_title,
+      "email_address":email_address,
+      "phone_number":phone_number,
+      "address":address
+    }
+    const send_to_server=()=>{
+    const isConfirmed = window.confirm('Are you sure you want to submit this data?');
+
+    if(isConfirmed){
+    fetch("http://127.0.0.1:5003/api/add_data",{
+      method: "POST",
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      body: JSON.stringify(send_data)
+    })
+    .then(response=> response.json())
+    .then((data)=>{
+      console.log("data response is",data)
+      setConfirm_status("successfuly added the add")
+    })
+    .catch(Error=>{
+      console.log("error occur",Error)
+      setConfirm_status("server is busy please try again")
+    })
+    }
+    else{
+      setConfirm_status("Submission cancelled check once")
+    }
+      }
+
+
+    let pattern=/^[^\s@]+@gmail\.com$/
+    let result = pattern.test(email_address);
+    if(result){
+      send_to_server()
+    }
+    else{
+      setConfirm_status("please check the email_address corretc")
+    }
+  }
+  return (
+    <div>
+      <form onSubmit={Handle_submit}>
+      employee_name: <input 
+            type='text'
+            value={employee_name}
+            onChange={Handle_employee_name}
+            required
+            />
+        department: <input 
+                type='text' 
+                value={department} 
+                onChange={Handle_department}
+                
+                />
+        job_title:  <input 
+            type='text'
+            value={job_title}
+            onChange={Handle_job_title}
+
+            />
+
+        email_address:<input 
+            type='email'
+            value={email_address}
+            onChange={Handle_email_address}
+            required
+            /> 
+        phone_number:<input 
+            type='integer'
+            value={phone_number}
+            onChange={Handle_phone_number}
+            required
+            />
+        address: <input 
+            type='text'
+            value={address}
+            onChange={Handle_address}
+            />
+            <br />
+        submit: <input 
+            type='submit'
+            />
+      </form>
+      <p>{confirm_status}</p>
+    </div>
+  )
+}
+
+export default Add_Data
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+import Route_1 from './Route_1';
+
+// Use createRoot to render the App component
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// root.render(<App />);
+root.render(<Route_1 />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
